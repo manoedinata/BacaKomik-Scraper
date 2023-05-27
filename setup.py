1b2b183031a3002db388797181c0e0c51fe6bffb
Hendra <manoedinata@gmail.com>
2023-05-27 13:22:54 +0700

    version: Bump: v0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bacakomikscraper",
-    version="0.2",
+    version="0.3",
     author="manoedinata",
     author_email="manoedinata@gmail.com",
     description="BacaKomik.co Scraper",
